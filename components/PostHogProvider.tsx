e2526f181a5d80d4ca807391ebc1f32df05ad0a5
indresh404 <indreshsuresh95@gmail.com>
2026-06-21 22:52:07 +0530

    added posthog monitoring

---
+++ components/PostHogProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import PostHogPageView from "./PostHogPageView";
+
+export default function PostHogProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      capture_pageview: false,
+      capture_pageleave: true,
+    });
+  }, []);
+
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  );
+}
